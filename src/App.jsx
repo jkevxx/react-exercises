b3d1e9781f35ff433c61a7896eb50e841cf8f667
Kevin <kevin.paez1010@gmail.com>
2023-04-18 21:51:15 -0600

    protected routes completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
-// import ApiRickAndMorty from "./components/consumeApiRickAndMorty/ApiRickAndMorty";
-// import Form from "./components/Form/Form";
-// import CrudApp from "./components/CRUDApp/CrudApp";
-
-// import { CrudApi } from "./crudAPI/components/CrudApi";
-
 // Exercise React Router
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-// import { Navbar } from "./reactRouter/components/Navbar";
-// import { AboutPage } from "./reactRouter/pages/AboutPage";
-// import { Dashboard } from "./reactRouter/pages/Dashboard";
-// import { HomePage } from "./reactRouter/pages/HomePage";
-// import { NotFoundPage } from "./reactRouter/pages/NotFoundPage";
-// import { UserPage } from "./reactRouter/pages/UserPage";
-// import { UsersPage } from "./reactRouter/pages/UsersPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // Exercise Protected Routes
-import Landing from './protectedRoutes/pages/Landing';
-import Home from './protectedRoutes/pages/Home';
-import Dashboard from './protectedRoutes/pages/Dashboard';
-import Analytic from './protectedRoutes/pages/Analytic';
-import Admin from './protectedRoutes/pages/Admin';
-import { Navbar } from './protectedRoutes/components/Navbar';
+import Landing from './pages/Landing';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Analytic from './pages/Analytic';
+import Admin from './pages/Admin';
+import { Navbar } from './components/Navbar';
 import { useState } from 'react';
-import ProtectedRoute from './protectedRoutes/components/ProtectedRoute';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -40,32 +27,6 @@ function App() {
   };
 
   return (
-    // <div>
-    //   {/* <ApiRickAndMorty /> */}
-    //   {/* <Form /> */}
-    //   {/* <CrudApp /> */}
-    //   {/* <CrudApi /> */}
-    // </div>
-
-    // Exercise React Router
-    // <BrowserRouter>
-    //   <Navbar />
-    //   <Routes>
-    //     <Route path="/" element={<HomePage />} />
-    //     <Route path="/about" element={<AboutPage />} />
-    //     <Route path="/users" element={<UsersPage />} />
-    //     <Route path="/usuarios" element={<Navigate to="/users" />} />
-    //     <Route path="/users/:id" element={<UserPage />} />
-
-    //     <Route path="/dashboard/*" element={<Dashboard />}>
-    //       <Route path="welcome" element={<p>Welcome</p>} />
-    //     </Route>
-
-    //     <Route path="*" element={<NotFoundPage />} />
-    //   </Routes>
-    // </BrowserRouter>
-
-    // Exercise Protected Routes
     <BrowserRouter>
       <Navbar />
 
